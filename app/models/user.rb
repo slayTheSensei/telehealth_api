ac3e1d3c10f58ad71a444b084de6f51352f5d5da
slayTheSensei <kylejpearson@gmail.com>
2017-09-04 11:41:26 -0400

    CONNECTED ALL TABLE RELATIONS
    connected doctor and user tables via appointments tables. I should be
    able to test the these with curl scripts next.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
+
 class User < ApplicationRecord
   include Authentication
   has_many :examples
+  has_many :appointments
+  has_many :doctors, through: :appointments
 end
